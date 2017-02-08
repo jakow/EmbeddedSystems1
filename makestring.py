2767e737084635825b7edd02511103b7142d015b
jakow <s1243303@sms.ed.ac.uk>
2017-02-08 11:15:03 +0000

    makestring changes

--- makestring.py
+++ makestring.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python
-out_lines = []
-with open('index.html', 'r') as in_file:
-	for line in in_file:
-		l = line.replace('\\', '\\\\')
-		l = l.replace('"', '\\"')
-		l = l.replace('\n', '')
 
-		if l: # remove empty lines
-			l = '"' + l + '\\n"\n'
-			out_lines.append(l)
+
+def make_string(fname):
+	out = []
+	with open(fname, 'r') as in_file:
+		for line in in_file:
+			l = line.replace('\\', '\\\\')
+			l = l.replace('"', '\\"')
+			l = l.replace('\n', '')
+
+			if l: # remove empty lines
+				l = '"' + l + '\\n"\n'
+				out.append(l)
+	return out
+
+def write_string(file, varname, content):
+	out_file.write("unsigned char {}[] = ".format(varname))
+	for line in content:
+		out_file.write(line)
+	out_file.write(";\n")
+
+webpage = make_string('index.html')
+style = make_string('style.css')
+script = make_string('script.js')
+
 
 
 with open('webpage.h', 'w') as out_file:
-	out_file.write("unsigned char WEBPAGE[] = ")
-	for line in out_lines:
-		out_file.write(line)
-	out_file.write(";")
+	write_string(out_file, "WEBPAGE", webpage)
+	write_string(out_file, "STYLE", style)
+	write_string(out_file, "SCRIPT", script)
+
+
